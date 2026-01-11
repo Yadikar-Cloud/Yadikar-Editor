9e71b2af0aa6cafdec3a7d06b5d6c6f476507f63
Keram <keramyasina@gmail.com>
2026-01-11 11:15:58 +0100

    page styling for mobile mode.

--- assets/editor.js
+++ assets/editor.js
@@ -19,7 +19,7 @@ function initializeTinyMCE(customSettings = {}, initialContent = '') {
         tools: { title: "Tools", items: "spellchecker | screenshot | code wordcount" },
         help: { title: "help", items: "help givefeedback | privacy terms" },
       },
-      content_style: `html {background: #ffffff; margin: 0;}`,    
+      content_style: `html {background: #ffffff; margin: 0;} body {padding: 0 10px;}`,    
     },
     plugins: "print preview paste importcss searchreplace autolink autosave save directionality code visualblocks visualchars fullscreen image link media template codesample table charmap hr pagebreak nonbreaking anchor toc insertdatetime advlist lists wordcount imagetools textpattern noneditable help charmap quickbars emoticons spellchecker suggestions grammerchecker cloudsignin openfromcomputer savetocomputer universaldrive screenshot settings pageview givefeedback",
     imagetools_cors_hosts: ["picsum.photos"],
